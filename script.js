b617f18f474c3a092c6a488dea8d107918402416
ajoke-naje <ajokenakinseye@gmail.com>
2024-08-19 20:57:10 -0400

    Add button and functionality to change grid dimensions

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
+const body = document.querySelector('body');
+
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const colDiv = document.createElement('div');
-        colDiv.classList.add('col');
-        rowDiv.appendChild(colDiv);
+const createGrid = function(n) {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row) => {
+        grid.removeChild(row);
+    });
+    for (let i = 0; i < n; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        for (let j = 0; j < n; j++) {
+            const colDiv = document.createElement('div');
+            colDiv.classList.add('col');
+            rowDiv.appendChild(colDiv);
+        }
+        grid.appendChild(rowDiv);
     }
-    grid.appendChild(rowDiv);
-}
-const changeCellColor = function (cell) {
-    cell.style.backgroundColor = 'blue';
-};
 
-const cells = document.querySelectorAll('.col');
-cells.forEach((cell) => {
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'blue';
-    })
+    const cells = document.querySelectorAll('.col');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'blue';
+        })
+    });
+}
+
+const div = document.createElement('div');
+div.classList.add('dimensionSelector');
+const btn = document.createElement('button');
+
+btn.textContent = 'GRID DIMENSIONS';
+btn.addEventListener('click', () => {
+    let dimension;
+    do {
+        dimension = +prompt('Enter a number (between 1 - 100) to create an n x n grid.');
+        if (dimension < 1) {
+            dimension = 1;
+        } else if (dimension > 100) {
+            dimension = 100;
+        }
+    } while (!dimension)
+    createGrid(dimension);
 });
+
+div.appendChild(btn);
+body.prepend(div);
+
+createGrid(16);
